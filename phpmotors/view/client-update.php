581dfea1becdc7f226fc9a0fa45a3d20e18cc059
abdulmahl <mahalanguma87@gmail.com>
2023-11-08 20:31:11 +0200

    .

--- phpmotors/view/client-update.php
+++ phpmotors/view/client-update.php
@@ -52,9 +52,9 @@
                     <h2 class="reg">Account Update</h2>
                    
                     <div class="registerNames__Field">
-                        <label for="clientFirstName" class="clientnames">First Name: <input name="firstname" type="text" id="clientFirstName"  <?php if(isset($firstname)) {echo $firstname;} elseif($_SESSION['clientData']['clientFirstname']) {echo $_SESSION['clientData']['clientFirstname'];} ?> placeholder="First Name*" required></label>
-                        <label for="clientLastName" class="clientnames">Last Name: <input name="lastname" type="text" id="clientLastName" <?php if(isset($lastname)) {echo $lastname;} elseif($_SESSION['clientData']['clientLastname']) {echo $_SESSION['clientData']['clientLastname'];} ?> placeholder="Last Name*" required></label>
-                        <label for="clientEmail" class="clientnames">Email: <input name="newEmail" type="email" id="clientEmail" <?php if(isset($newEmail)) {echo $newEmail;} elseif($_SESSION['clientData']['clientEmail']) {echo $_SESSION['clientData']['clientEmail'];} ?> placeholder="Email*" required></label>
+                        <label for="clientFirstName" class="clientnames">First Name: <input name="firstname" type="text" id="clientFirstName"  placeholder="First Name*" required <?php if(isset($firstname)) {echo "value='$firstname'";} elseif($_SESSION['clientData']['clientFirstname']) {echo "value='".$_SESSION['clientData']['clientFirstname']."'";} ?>></label>
+                        <label for="clientLastName" class="clientnames">Last Name: <input name="lastname" type="text" id="clientLastName" placeholder="Last Name*" required <?php if(isset($lastname)) {echo "value='$lastname'";} elseif($_SESSION['clientData']['clientLastname']) {echo "value='".$_SESSION['clientData']['clientLastname']."'";} ?>></label>
+                        <label for="clientEmail" class="clientnames">Email: <input name="newEmail" type="email" id="clientEmail"  placeholder="Email*" required <?php if(isset($newEmail)) {echo "value='$newEmail'";} elseif($_SESSION['clientData']['clientEmail']) {echo "value='".$_SESSION['clientData']['clientEmail']."'";} ?>></label>
                     </div>
 
                     <div id="regButton">
